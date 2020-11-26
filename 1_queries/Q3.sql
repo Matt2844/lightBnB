dfdcdb273b005bcc053f9b3b574cc00208549dca
Matt <matt.hd.ford@gmail.com>
2020-11-26 21:33:53 +0000

    updating user database with sql

--- 1_queries/Q3.sql
+++ 1_queries/Q3.sql
@@ -1,11 +1,12 @@
 -- Question 3 
 
-SELECT properties.id, title, city, cost_per_night, AVG(rating) as average_rating 
+SELECT properties.*, avg(property_reviews.rating) as average_rating
 FROM properties
 JOIN property_reviews 
-ON properties.id = property_reviews.property_id
-WHERE city = 'Vancouver' AND AVG(rating) >= 4
+ON properties.id = property_id
+WHERE city LIKE '%ancouv%'
 GROUP BY properties.id
-ORDER BY cost_per_night ASC
+HAVING avg(property_reviews.rating) >= 4
+ORDER BY cost_per_night
 LIMIT 10;
 
